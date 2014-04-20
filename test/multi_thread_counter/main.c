d61cfb31c9bb5dd493c686fa078f355367664e70
Josh McGrath <josh.mcgrath08@gmail.com>
2014-04-19 21:21:22 -0400

    simple multi-threaded example

---
+++ test/multi_thread_counter/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <assert.h>
+
+#define NUM_THREADS 5
+
+int num_threads = 0;
+
+pthread_mutex_t bad_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void * child_code (void *a) {
+  int __sm_thread_id = *(int *) a;
+  if (__sm_thread_id == (NUM_THREADS - 1)
+      && (random() % NUM_THREADS == (NUM_THREADS - 1)))
+    num_threads +=1;
+  else {
+    pthread_mutex_lock(&bad_mutex);
+    num_threads += 1;
+    pthread_mutex_unlock(&bad_mutex);
+  }
+}
+
+int test_main (void) {
+  int __sm_thread_id = -1;
+  num_threads = 0;
+  int *child_num = (int *) malloc(NUM_THREADS * sizeof(int));
+  int i;
+  for (i = 0; i < NUM_THREADS; i++)
+    *(child_num + i) = i;
+
+  pthread_t *ts = malloc (NUM_THREADS * sizeof(pthread_t));
+  for (i = 0; i < NUM_THREADS; i++)
+    pthread_create((ts + i), NULL, child_code, child_num + i);
+
+  void **ret = NULL;
+  for (i = 0; i < NUM_THREADS; i++)
+    pthread_join(*(ts + i), ret);
+
+  assert(num_threads == NUM_THREADS);
+
+  free(child_num);
+  free(ts);
+}
